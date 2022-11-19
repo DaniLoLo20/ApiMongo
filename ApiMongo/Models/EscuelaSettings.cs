0eba7acbd3f208e49d45488cda82ab2ce566899b
DaniLoLo20 <67060554+DaniLoLo20@users.noreply.github.com>
2022-11-19 11:13:11 -0600

    Agregar archivos de proyecto.

---
+++ ApiMongo/Models/EscuelaSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace ApiMongo.Models
+{
+    public class EscuelaSettings :IEscuelaSttings
+    {
+        public string Server { get; set; }
+        public string DataBase { get; set; }
+
+        public string Collection { get; set; }
+    }
+
+    public interface IEscuelaSttings
+    {
+
+        string Server { get; set; }
+        string DataBase { get; set; }
+
+        string Collection { get; set; }
+
+    }
+}
